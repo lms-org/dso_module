84a319c2becf26225ac57d404e5519f43c9c789c
phibedy <philipp.schmette@gmx.de>
2016-11-14 11:56:49 +0100

    WIP

--- include/dsodometry.h
+++ include/dsodometry.h
@@ -4,11 +4,17 @@
 #include <lms/module.h>
 #include <lms/imaging/image.h>
 
+//dso
+#include "util/settings.h"
+#include "FullSystem/FullSystem.h"
+
 /**
  * @brief LMS module dsodometry
  **/
 class Dsodometry : public lms::Module {
     lms::ReadDataChannel<lms::imaging::Image> image;
+    int frameID;
+    dso::FullSystem* fullSystem;
 public:
     bool initialize() override;
     bool deinitialize() override;
--- src/dsodometry.cpp
+++ src/dsodometry.cpp
@@ -1,14 +1,30 @@
 #include "dsodometry.h"
-#include "dsodometry.h"
-
-#include "util/settings.h"
-#include "FullSystem/FullSystem.h"
 
 
-using namespace dso;
 
 bool Dsodometry::initialize() {
     image = readChannel<lms::imaging::Image>("IMAGE");
+
+    dso::setting_desiredImmatureDensity = 1000;
+    dso::setting_desiredPointDensity = 1200;
+    dso::setting_minFrames = 5;
+    dso::setting_maxFrames = 7;
+    dso::setting_maxOptIterations=4;
+    dso::setting_minOptIterations=1;
+    dso::setting_logStuff = false;
+    dso::setting_kfGlobalWeight = 1.3;
+
+
+    //printf("MODE WITH CALIBRATION, but without exposure times!\n");
+    //setting_photometricCalibration = 2;
+
+    dso::setting_photometricCalibration = 0; //no calibration given
+    dso::setting_affineOptModeA = 0;
+    dso::setting_affineOptModeB = 0;
+    //TODO do we need to set setting_useExposure = 0;
+
+    fullSystem = new dso::FullSystem();
+    fullSystem->linearizeOperation=false;
     return true;
 }
 
@@ -17,28 +33,34 @@ bool Dsodometry::deinitialize() {
 }
 
 bool Dsodometry::cycle() {
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8);
-        assert(cv_ptr->image.type() == CV_8U);
-        assert(cv_ptr->image.channels() == 1);
-
-
-        if(setting_fullResetRequested)
-        {
-            IOWrap::Output3DWrapper* wrap = fullSystem->outputWrapper;
+    /*
+        if(setting_fullResetRequested){
+            dso::IOWrap::Output3DWrapper* wrap = fullSystem->outputWrapper;
             delete fullSystem;
             if(wrap != 0) wrap->reset();
             fullSystem = new FullSystem();
             fullSystem->linearizeOperation=false;
             fullSystem->outputWrapper = wrap;
+
             if(undistorter->photometricUndist != 0)
                 fullSystem->setGammaFunction(undistorter->photometricUndist->getG());
-            setting_fullResetRequested=false;
-        }
 
-        MinimalImageB minImg((int)cv_ptr->image.cols, (int)cv_ptr->image.rows,(unsigned char*)cv_ptr->image.data);
-        ImageAndExposure* undistImg = undistorter->undistort<unsigned char>(&minImg, 1,0, 1.0f);
-        fullSystem->addActiveFrame(undistImg, frameID);
+            dso::setting_fullResetRequested=false;
+        }
+    */
+        //dso::MinimalImageB minImg(image->width(),image->height(),const_cast<unsigned char*>(image->data())); //yay const cast
+        dso::ImageAndExposure *myImage = new dso::ImageAndExposure(image->width(),image->height());
+        for(int x = 0; x < image->width(); x++){
+            for(int y = 0; y<image->height(); y++){
+                myImage->image[x+y*image->width()] = (float)image->data()[x+y*image->width()];
+            }
+        }
+        fullSystem->addActiveFrame(myImage, frameID);
         frameID++;
-    delete undistImg;
+        if(fullSystem->isLost){
+            logger.error("system is lost");
+            return false;
+        }
+        //TODO how do we get the position from it?
     return true;
 }
