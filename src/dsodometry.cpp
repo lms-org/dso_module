bd840393f24ea353677e7210414b7dfc7d27237e
phibedy <philipp.schmette@gmx.de>
2016-11-22 15:27:14 +0100

    WIP, fullsystem init fails

--- src/dsodometry.cpp
+++ src/dsodometry.cpp
@@ -1,10 +1,12 @@
 #include "dsodometry.h"
-
+#include "MyIOWrapper.h"
 
 
 bool Dsodometry::initialize() {
     image = readChannel<lms::imaging::Image>("IMAGE");
+    frameID = 0;
 
+    //set some default values
     dso::setting_desiredImmatureDensity = 1000;
     dso::setting_desiredPointDensity = 1200;
     dso::setting_minFrames = 5;
@@ -15,52 +17,61 @@ bool Dsodometry::initialize() {
     dso::setting_kfGlobalWeight = 1.3;
 
 
-    //printf("MODE WITH CALIBRATION, but without exposure times!\n");
-    //setting_photometricCalibration = 2;
-
+    //disable calibration (just for testing)
     dso::setting_photometricCalibration = 0; //no calibration given
     dso::setting_affineOptModeA = 0;
     dso::setting_affineOptModeB = 0;
-    //TODO do we need to set setting_useExposure = 0;
 
+    //TODO do we need to set
+    dso::setting_useExposure = 0;
+
+    //create dso fullsystem
     fullSystem = new dso::FullSystem();
     fullSystem->linearizeOperation=false;
+    //TODO what to do as we get the image from the camera? fullSystem->setGammaFunction(reader->getPhotometricGamma());
+    //add the IOWrapper
+    fullSystem->outputWrapper.push_back(new dso::IOWrap::SampleOutputWrapper());
+
+    if(!fullSystem->initFailed){
+        logger.error("init")<<"fullSystem initFailed";
+        return false;
+    }
     return true;
 }
 
 bool Dsodometry::deinitialize() {
+    logger.info("deinitialize")<<"waiting for dso";
+    fullSystem->blockUntilMappingIsFinished();
+    fullSystem->printResult("dso_result.txt");
+    logger.info("deinitialize")<<"printed dso result";
     return true;
 }
 
 bool Dsodometry::cycle() {
-    /*
-        if(setting_fullResetRequested){
-            dso::IOWrap::Output3DWrapper* wrap = fullSystem->outputWrapper;
-            delete fullSystem;
-            if(wrap != 0) wrap->reset();
-            fullSystem = new FullSystem();
-            fullSystem->linearizeOperation=false;
-            fullSystem->outputWrapper = wrap;
+    if(!fullSystem->initialized){
+        logger.error("cycle")<<"fullSystem not initialized";
+        return false;
+    }
+    //get the image from the camera
+    dso::ImageAndExposure *myImage = new dso::ImageAndExposure(image->width(),image->height());
+    for(int x = 0; x < image->width(); x++){
+        for(int y = 0; y<image->height(); y++){
+            myImage->image[x+y*image->width()] = (float)image->data()[x+y*image->width()];
+        }
+    }
+    //Add the new image
+    logger.debug("adding new image, id")<<frameID;
+    fullSystem->addActiveFrame(myImage, frameID);
+    frameID++;
 
-            if(undistorter->photometricUndist != 0)
-                fullSystem->setGammaFunction(undistorter->photometricUndist->getG());
-
-            dso::setting_fullResetRequested=false;
-        }
-    */
-        //dso::MinimalImageB minImg(image->width(),image->height(),const_cast<unsigned char*>(image->data())); //yay const cast
-        dso::ImageAndExposure *myImage = new dso::ImageAndExposure(image->width(),image->height());
-        for(int x = 0; x < image->width(); x++){
-            for(int y = 0; y<image->height(); y++){
-                myImage->image[x+y*image->width()] = (float)image->data()[x+y*image->width()];
-            }
-        }
-        fullSystem->addActiveFrame(myImage, frameID);
-        frameID++;
-        if(fullSystem->isLost){
-            logger.error("system is lost");
-            return false;
-        }
-        //TODO how do we get the position from it?
+    //check if the system is lost
+    if(fullSystem->isLost){
+        logger.error("system is lost");
+        return false;
+    }
+    //print the results
+    std::string myString;
+    fullSystem->printResult(myString);
+    logger.error("result: ")<<myString; //empty :/
     return true;
 }
